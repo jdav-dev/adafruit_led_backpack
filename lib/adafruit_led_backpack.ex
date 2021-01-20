9f7f9c331acab7d3a7b765aa1b9451934834eab0
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2021-01-19 20:50:53 -0500

    Initial commit
    Result of running `mix new --sup adafruit_led_backpack`

---
+++ lib/adafruit_led_backpack.ex
@@ -0,0 +1,18 @@
+defmodule AdafruitLedBackpack do
+  @moduledoc """
+  Documentation for `AdafruitLedBackpack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdafruitLedBackpack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/adafruit_led_backpack_test.exs
@@ -0,0 +1,8 @@
+defmodule AdafruitLedBackpackTest do
+  use ExUnit.Case
+  doctest AdafruitLedBackpack
+
+  test "greets the world" do
+    assert AdafruitLedBackpack.hello() == :world
+  end
+end
